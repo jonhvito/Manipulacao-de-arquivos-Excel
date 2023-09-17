beedb1d053487e884be8774ce911f3a3dbaac4c1
jonh vito <victorjoao817@gmail.com>
2023-09-17 14:25:27 -0300

    refactored code

--- main.py
+++ main.py
@@ -1,87 +1,102 @@
 from datetime import date
-
 from openpyxl.chart import LineChart, Reference
 from openpyxl.drawing.image import Image
-from openpyxl.styles import Font
-from openpyxl.styles import PatternFill, Alignment
+from openpyxl.styles import Font, PatternFill, Alignment
 from openpyxl.workbook import Workbook
-
-# acao = input("Qual o código da Ação que você que processar? ").upper()
-acao = "BIDI4"
-with open(f'Dados/{acao}.txt', 'r') as arquivo_cotacao:
-    linhas = arquivo_cotacao.readlines()
-    linhas = [linha.replace("\n", "").split(";") for linha in linhas]
-
-workbook = Workbook()
-planilha_ativa = workbook.active
-planilha_ativa.title = "Dados"
-
-planilha_ativa.append(["DATA", "COTAÇÃO", "BANDA INFERIOR", "BANDA SUPERIOR"])
-
-indice = 2
-
-for linha in linhas:
-    # Data
-    ano_mes_dia = linha[0].split(" ")[0]
-    data = date(
-        year=int(ano_mes_dia.split("-")[0]),
-        month=int(ano_mes_dia.split("-")[1]),
-        day=int(ano_mes_dia.split("-")[2]))
-    # Cotação
-    cotacao = float(linha[1])
-
-    # Atualiza as células da planilha excel
-    planilha_ativa[f'A{indice}'] = data
-    planilha_ativa[(f'B{indice}')] = cotacao
-    planilha_ativa[f'C{indice}'] = f'=AVERAGE(B{indice}:B{indice + 19}) - 2*STDEV(B{indice}:B{indice +19})'
-    planilha_ativa[f'D{indice}'] = f'=AVERAGE(B{indice}:B{indice + 19}) + 2*STDEV(B{indice}:B{indice + 19})'
-    indice += 1
+from classes import LeitorAcoes, PropriedadeSerieGrafico, GerenciadorPlanilha
 
 
-planilha_grafico = workbook.create_sheet("Gráfico")
-workbook.active = planilha_grafico
+try:
+    # acao = input("Qual o código da Ação que você que processar? ").upper()
+    acao = "BIDI4"
 
-# Mesclagem de células
-planilha_grafico.merge_cells("A1:T2")
-cabecalho = planilha_grafico['A1']
-cabecalho.font = Font(b=True, sz=18, color="FFFFFF")
-cabecalho.fill = PatternFill("solid", fgColor="07838f")
-cabecalho.alignment = Alignment(vertical="center", horizontal="center")
-cabecalho.value = "Histórico de Cotações"
-
-grafico = LineChart()
-grafico.width = 33.87
-grafico.height = 14.82
-grafico.title = f"Cotações - {acao}"
-grafico.x_axis.title = "Data da Cotação"
-grafico.y_axis.title = "Valor da Cotação"
-
-referencia_cotacoes = Reference(planilha_ativa, min_col=2, min_row=2, max_col=4, max_row=indice)
-referencia_datas = Reference(planilha_ativa, min_col=1, min_row=2, max_col=1, max_row=indice)
-grafico.add_data(referencia_cotacoes)
-grafico.set_categories(referencia_datas)
-
-
-linha_cotacoes = grafico.series[0]
-linha_bb_inferior = grafico.series[1]
-linha_bb_sup = grafico.series[2]
-
-linha_cotacoes.graphicalProperties.line.width = 0
-linha_cotacoes.graphicalProperties.line.solidFill = '0a55ab'
-
-linha_bb_inferior.graphicalProperties.line.width = 0
-linha_bb_inferior.graphicalProperties.line.solidFill = 'a61508'
-
-linha_bb_sup.graphicalProperties.line.width = 0
-linha_bb_sup.graphicalProperties.line.solidFill = '12a154'
+    leitor_acoes = LeitorAcoes(caminho_arquivo='./Dados/')
+    leitor_acoes.processa_arquivo(acao)
 
 
 
-planilha_grafico.add_chart(grafico, "A3")
+    gerenciador = GerenciadorPlanilha()
+    planilha_dados = gerenciador.adiciona_planilha("Dados")
 
-imagem = Image('./recursos/logo.png')
-planilha_grafico.merge_cells("I32:L35")
-planilha_grafico.add_image(imagem, 'I32')
+    gerenciador.adiciona_linha(["DATA", "COTAÇÃO", "BANDA INFERIOR", "BANDA SUPERIOR"])
+
+    indice = 2
+
+    for linha in leitor_acoes.dados:
+        # Data
+        ano_mes_dia = linha[0].split(" ")[0]
+        data = date(
+            year=int(ano_mes_dia.split("-")[0]),
+            month=int(ano_mes_dia.split("-")[1]),
+            day=int(ano_mes_dia.split("-")[2]))
+        # Cotação
+        cotacao = float(linha[1])
+
+        formula_bb_inferior = f'=AVERAGE(B{indice}:B{indice + 19}) - 2*STDEV(B{indice}:B{indice +19})'
+        formula_bb_superior = f'=AVERAGE(B{indice}:B{indice + 19}) + 2*STDEV(B{indice}:B{indice + 19})'
+
+        # Atualiza as células da planilha excel
+        gerenciador.atualiza_celula(celula=f'A{indice}', dado = data)
+        gerenciador.atualiza_celula(celula=f'B{indice}', dado = cotacao)
+        gerenciador.atualiza_celula(celula=f'C{indice}', dado = formula_bb_inferior)
+        gerenciador.atualiza_celula(celula=f'D{indice}', dado = formula_bb_superior)
 
 
-workbook.save("./saida/Planilha.xlsx")
+        indice += 1
+
+
+    gerenciador.adiciona_planilha(titulo_planilha='Gráfico')
+
+
+    # Mesclagem de células
+    gerenciador.mescla_celulas(celula_inicio='A1', celula_fim='T2')
+
+    gerenciador.aplica_estilos(
+        celula='A1',
+        estilos=[
+            ('font', Font(b=True, sz=18, color="FFFFFF")),
+            ('alignment',Alignment(vertical="center", horizontal="center")),
+            ('fill', PatternFill("solid", fgColor="07838f")),
+        ]
+    )
+
+    gerenciador.atualiza_celula('A1', "Histórico de Cotações")
+
+
+    referencia_cotacoes = Reference(planilha_dados, min_col=2, min_row=2, max_col=4, max_row=indice)
+    referencia_datas = Reference(planilha_dados, min_col=1, min_row=2, max_col=1, max_row=indice)
+
+
+    gerenciador.adiciona_grafico_linha(
+        celula='A3',
+        comprimento=33.87,
+        altura=14.82,
+        titulo=f"Cotações - {acao}",
+        titulo_eixo_x="Data da Cotação",
+        titulo_eixo_y="Valor da Cotação",
+        referencia_eixo_x=referencia_cotacoes,
+        referencia_eixo_y=referencia_datas,
+        propriedades_grafico=[
+            PropriedadeSerieGrafico(grossura=0, cor_preechimento='0a55ab'),
+            PropriedadeSerieGrafico(grossura=0, cor_preechimento='d115a8'),
+            PropriedadeSerieGrafico(grossura=0, cor_preechimento='ff1a05'),]
+    )
+
+
+    gerenciador.mescla_celulas(celula_inicio='I32',celula_fim='L35')
+    gerenciador.adiciona_imagem(celula='I32', caminho_imagem='./recursos/logo.png')
+
+
+    gerenciador.salva_arquivo('./saida/Planilha.xlsx')
+
+except ValueError:
+    print(" \n Formato dos dados incorreto! Favor verificar. \n")
+
+except AttributeError:
+    print(" \n Atributo inexistente! \n")
+
+except FileNotFoundError:
+    print(" \n Arquivo Não Encontado \n")
+
+except Exception as excecao:
+    print(f'\n Ocorreu um erro na execução do programa. Erro: {str(excecao)} \n')
